0436aeb554d90459a38a5e6c1e237c7cb1d33969
samuelconlon <conlonsp@yahoo.com>
2022-12-15 18:31:58 -0500

    organized code in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,26 +9,12 @@ import '../App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-        </Switch>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
