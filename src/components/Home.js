2bf94872a6efefd1b87c152bafeeab27d71db627
samuelconlon <conlonsp@yahoo.com>
2022-12-15 18:32:39 -0500

    added code from App.js to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,23 @@ import React from 'react'
 function Home() {
 
   return (
-    <div>I'm home</div>
+    <div>
+      <div>I'm Home</div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   )
 }
 
