6b5113c169b80ae514def885f11c3527c5a905dd
samuelconlon <conlonsp@yahoo.com>
2022-12-17 14:02:04 -0500

    updated imports in App.js with new component names

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,15 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
 
-import NavBar from './NavBar'
-import Home from './Header'
-import FindHome from './PostForm'
-import ListHome from './ListingsContainer'
+import Header from './Header'
+import PostForm from './PostForm'
+import ListingsContainer from './ListingsContainer'
 
 import '../App.css';
 
 function App() {
   return (
     <div className="App-header">
-      <NavBar />
-      <Switch>
-        <Route path='/list'>
-          <ListHome />
-        </Route>
-        <Route path='/find'>
-          <FindHome />
-        </Route>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      
     </div>
   );
 }
