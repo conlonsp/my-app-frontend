7c5fba25d9529816b3b862108a42d5df5908566b
samuelconlon <conlonsp@yahoo.com>
2022-12-18 11:36:08 -0500

    get request and set state

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,19 @@ import ListingsContainer from './ListingsContainer'
 import '../App.css';
 
 function App() {
+  const [homes, setHomes] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:9392/homes")
+    .then(r => r.json)
+    .then(homes => setHomes(homes))
+  })
+  
   return (
     <div className="App-header">
       <Header />
       <PostForm />
-      <ListingsContainer />
+      <ListingsContainer homes={homes}/>
     </div>
   );
 }
