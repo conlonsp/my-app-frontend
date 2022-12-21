c6c1586138e585cd2fad7333d1c9468d018d4fcf
samuelconlon <conlonsp@yahoo.com>
2022-12-21 15:09:33 -0500

    added handleClick and handleUpdate

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -4,6 +4,14 @@ function Listing({ home }) {
 
   const { address, price, square_feet, agent_id, created_at, updated_at } = home
 
+  function handleDelete() {
+    alert('do you want to delete?')
+  }
+
+  function handleUpdate() {
+    alert('do you want to update')
+  }
+
   return (
     <div>
       <br/>
@@ -11,6 +19,8 @@ function Listing({ home }) {
         {address} | Price: ${price} | Square Feet: {square_feet}
         <br/>
         Listed on: {created_at}
+        <button onClick={handleUpdate}>Update</button>
+        <button onClick={handleDelete}>Delete</button>
       </div>
     </div>
   )
