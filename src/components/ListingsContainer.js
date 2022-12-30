a2eca6a294f4f704983f617686916ad0b83f0801
samuelconlon <conlonsp@yahoo.com>
2022-12-30 14:28:53 -0500

    passed homes and setHomes to Listing in ListingContainer

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Listing from './Listing'
 
-function ListingsContainer({ homes, onHomeDelete }) {
+function ListingsContainer({ homes, setHomes, onHomeDelete }) {
   
   return (
     <div>
       {homes.map(home => {
         return (
-          <Listing key={home.id} home={home} onHomeDelete={onHomeDelete} />
+          <Listing
+            key={home.id}
+            home={home}
+            onHomeDelete={onHomeDelete}
+            homes={homes}
+            setHomes={setHomes}
+          />
         )
       })}
     </div>
