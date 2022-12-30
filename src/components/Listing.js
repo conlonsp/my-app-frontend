fa97e4b57309c319d82f48d4e6b2e1cfe9e0f555
samuelconlon <conlonsp@yahoo.com>
2022-12-30 13:42:46 -0500

    imported useState and created isUpdate state in Listing.js

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Listing({ home, onHomeDelete }) {
+  const [isUpdate, setIsUpdate] = useState(false)
 
   const { id, address, price, square_feet, agent_id, created_at, updated_at } = home
 
@@ -12,8 +13,8 @@ function Listing({ home, onHomeDelete }) {
     onHomeDelete(id)
   }
 
-  function handleUpdate() {
-    alert('do you want to update?')
+  function handleUpdateButton() {
+    
   }
 
   return (
@@ -23,7 +24,7 @@ function Listing({ home, onHomeDelete }) {
         {address} | Price: ${price} | Square Feet: {square_feet}
         <br/>
         Listed on: {created_at}
-        <button onClick={handleUpdate}>Update</button>
+        <button onClick={handleUpdateButton}>Update</button>
         <button onClick={handleDelete}>Delete</button>
       </div>
     </div>
