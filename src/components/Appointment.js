20aec30396815819e22e7596b395f518b907f752
samuelconlon <conlonsp@yahoo.com>
2023-01-02 17:13:03 -0500

    added home address to appointments in Appointment

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Appointment({ appt, homes, onApptDelete }) {
-  const {id, scheduler, time} = appt
+  const {id, scheduler, time, home_id} = appt
 
   function handleDelete() {
     fetch(`http://localhost:9492/appointments/${id}`, {
@@ -10,10 +10,11 @@ function Appointment({ appt, homes, onApptDelete }) {
     onApptDelete(id)
   }
 
+  const findHomeAddress = homes.find(home => home.id === home_id).address
+
   return (
     <div>
-      {/* {findHomeByAppt.address} */}
-      {scheduler}
+      {scheduler} at {time} at {findHomeAddress}
       <button onClick={handleDelete}>Delete</button>
     </div>
 
