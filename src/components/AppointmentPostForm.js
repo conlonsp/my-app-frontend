603324f298229a61d8fcd397f476dae0877640c3
samuelconlon <conlonsp@yahoo.com>
2023-01-02 19:23:52 -0500

    created button to show and hide AppointmentPostForm

--- src/components/AppointmentPostForm.js
+++ src/components/AppointmentPostForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 function AppointmentPostForm({ appointments, setAppointments }) {
+  const [showForm, setShowForm] = useState(false)
   const [createAppointment, setCreateAppointment] = useState({
     scheduler: '',
     time: '',
@@ -40,44 +41,55 @@ function AppointmentPostForm({ appointments, setAppointments }) {
     })
   }
 
+  function handleShowForm() {
+    setShowForm(!showForm)
+  }
+
   return (
     <div>
-      <h2>Schedule an Appointment</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Your Name: </label>
-        <input
-          type='text'
-          name='scheduler'
-          placeholder='ex: Joe Shmoe'
-          onChange={handleChange}
-          value={createAppointment.scheduler}
-        />
-        <label>Date/Time: </label>
-        <input
-          type='text'
-          name='time'
-          placeholder='ex: January 01, 2023 06:31'
-          onChange={handleChange}
-          value={createAppointment.time}
-        />
-        <label>Home ID: </label>
-        <input
-          type='text'
-          name='home_id'
-          placeholder='ex: 7'
-          onChange={handleChange}
-          value={createAppointment.home_id}
-        />
-        <label>Agent ID: </label>
-        <input
-          type='text'
-          name='agent_id'
-          placeholder='ex: 13'
-          onChange={handleChange}
-          value={createAppointment.agent_id}
-        />
-        <button>Submit</button>
-      </form>
+      {!showForm ?
+        <button onClick={handleShowForm}>Schedule Appt</button>
+        :
+        <div>
+          <h2>Schedule an Appointment</h2>
+          <form onSubmit={handleSubmit}>
+            <label>Your Name: </label>
+            <input
+              type='text'
+              name='scheduler'
+              placeholder='ex: Joe Shmoe'
+              onChange={handleChange}
+              value={createAppointment.scheduler}
+            />
+            <label>Date/Time: </label>
+            <input
+              type='text'
+              name='time'
+              placeholder='ex: January 01, 2023 06:31'
+              onChange={handleChange}
+              value={createAppointment.time}
+            />
+            <label>Home ID: </label>
+            <input
+              type='text'
+              name='home_id'
+              placeholder='ex: 7'
+              onChange={handleChange}
+              value={createAppointment.home_id}
+            />
+            <label>Agent ID: </label>
+            <input
+              type='text'
+              name='agent_id'
+              placeholder='ex: 13'
+              onChange={handleChange}
+              value={createAppointment.agent_id}
+            />
+            <button>Submit</button>
+          </form>
+          <button onClick={handleShowForm}>Hide</button>
+        </div>
+      }
     </div>
   )
 }
