9451af1de4f2a53bab720880fdb439dcd661acea
samuelconlon <conlonsp@yahoo.com>
2023-01-10 17:15:27 -0500

    added styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,49 @@ import { NavLink } from 'react-router-dom'
 
 function NavBar() {
 
+  const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "white",
+    textDecoration: "none",
+    color: "darkslategray",
+  };
+
   return (
     <div>
-      <NavLink to='/' exact>
+      <NavLink 
+        to='/'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'darkslategray',
+          color: 'white',
+        }}
+      >
         Home
       </NavLink>
-      <NavLink to='/listings'>
+      <NavLink
+        to='/listings'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'darkslategray',
+          color: 'white',
+        }}
+      >
         Listings
       </NavLink>
-      <NavLink to='/agents'>
+      <NavLink
+        to='/agents'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'darkslategray',
+          color: 'white',
+        }}
+      >
         Agents
       </NavLink>
     </div>
