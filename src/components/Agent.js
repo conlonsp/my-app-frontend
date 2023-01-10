ba741d1772cf69851b603347be1400da0eb9ad60
samuelconlon <conlonsp@yahoo.com>
2023-01-10 14:51:29 -0500

    added isClicked state in Agent

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -3,6 +3,8 @@ import React from 'react'
 function Agent({ agent, onAgentDelete }) {
   const {id, name, brokerage, email, phone_number} = agent
 
+  const [isClicked, setIsClicked] = useState(false)
+
   function handleDelete() {
     fetch(`http://localhost:9492/agents/${id}`, {
       method: 'DELETE',
